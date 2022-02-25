43d6e80c5d0fecc3dbf28f1c402b0bb4317405b4
Cristian Sousa <eng.cristiansousa@gmail.com>
2022-02-25 11:28:34 -0300

    Adicionado sistema de login primário

--- livros/views.py
+++ livros/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from usuarios.models import Usuario
 
 # Create your views here.
 
-def cadastrar(request):
-    return HttpResponse('Olá')
+def home(request):
+    if request.session.get('usuario'):
+        usuario = Usuario.objects.get(id=request.session['usuario'])
+        return HttpResponse(f"Olá {usuario.nome}")
+    else:
+        return redirect("auth/login/?status=2")
